ea0e994be464eadaccb4fccd57705d11a2d909b9
Praveen Kumar Yadav <praveen6206yadav@gmail.com>
2025-08-26 14:24:21 +0530

    first commit - Paytm clone

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const headerBtnImg = document.getElementById("header-btn-img");
+
+
+const onHover = (e) => (headerBtnImg.src = "../assets/user-2.svg");
+
+const onLeave = (e) => (headerBtnImg.src = "../assets/user-default.svg");
+
+
+const carausal = document.querySelector(".section-14-child-right");
+
+const leftBtn = document.getElementById("left-btn");
+
+const rightBtn = document.getElementById("right-btn");
+
+const list = document.querySelector(".section-14-child-right-cards");
+
+const onClickRight = () => {
+    carausal.scrollTo(list.scrollWidth, 0);
+    rightBtn.style.opacity = 0;
+    leftBtn.style.opacity = 1;
+};
+
+const onClickLeft = () => {
+    carausal.scrollTo(-list.scrollWidth, 0);
+    rightBtn.style.opacity = 1;
+    leftBtn.style.opacity = 0;
+};
+
+
+
+const carausal2 = document.getElementById("part2");
+
+const leftBtn2 = document.getElementById("left-btn2");
+
+const rightBtn2 = document.getElementById("right-btn2");
+
+const list2 = document.getElementById("cards");
+
+const onClickRight2 = () => {
+    carausal2.scrollTo(list2.scrollWidth, 0);
+    rightBtn2.style.opacity = 0;
+    leftBtn2.style.opacity = 1;
+};
+
+const onClickLeft2 = () => {
+    carausal2.scrollTo(-list2.scrollWidth, 0);
+    rightBtn2.style.opacity = 1;
+    leftBtn2.style.opacity = 0;
+};
